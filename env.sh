ab2e79620c82f97e014110f19b7dc407feb4cda5
RayanDev5M <rayan2.0.stars@gmail.com>
2025-11-10 15:12:43 +0100

    Add files via upload

---
+++ env.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export DRAFTBOT_CHANNEL_ID="1437408466643961146"
+export DRAFTBOT_ID="318312854816161792"
+export NOTIF_CHANNEL_ID="1437408883906969630"
+export DISCORD_TOKEN="MTQzNTc0NzkzMjgzMzA1ODgxNg.G_OREJ.bK0RNU0s0xUjEtLzqj9XLgQECJw1ULLJ9yJR-g"
+export DISCORD_CLIENT_ID="1435747932833058816"
+
+export API_URL="https://zeroprice.alwaysdata.net/index.php"
+export API_KEY="ton_api_key_super_secure_xyz123"
+export JWT_SECRET="your_jwt_secret_key_change_this"
